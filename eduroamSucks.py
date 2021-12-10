e42afbbcd282b0e157617f04b73ffd940e617507
Matthew McGoldrick <75187200+McGoldrick11@users.noreply.github.com>
2021-12-09 21:59:56 -0500

    add comments to script

--- eduroamSucks.py
+++ eduroamSucks.py
@@ -1,8 +1,11 @@
 import subprocess
+# used for interfacing python with cmd terminal
 
 dnsFlush = "ipconfig /flushdns"
 subprocess.run(dnsFlush, capture_output=True, text=True).stdout
+# clear the dns cache
 
 reconnect = "netsh wlan connect ssid=eduroam name=eduroam"
 subprocess.run(reconnect, capture_output=True, text=True).stdout
+# restart the wifi connection
 
